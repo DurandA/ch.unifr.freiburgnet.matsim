71cd58f875c4c4f384fe3252c011ea77f5d5bc60
Arnaud Durand <arnaud.durand@unifr.ch>
2014-03-20 23:55:36 +0100

    first commit

---
+++ src/playground/MyFirstControler.java
@@ -0,0 +1,17 @@
+package playground;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+
+public class MyFirstControler {
+
+	public static void main(String[] args) {
+		Config config = ConfigUtils.loadConfig("input/config.xml");
+		Controler controler = new Controler(config);
+		
+		// controler.setOverwriteFiles(true);
+		controler.run();
+	}
+
+}
